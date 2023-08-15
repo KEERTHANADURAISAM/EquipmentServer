b7590e1f0c8c425ecf337307f8d3644d78b10f25
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2023-08-15 15:01:31 +0530

    url

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.post("/user", async function (req, res) {
   }
 });
 
-app.get("/users", async function (req, res) {
+// all get requests in Rental items
+
+app.get("/treadmillimgs", async function (req, res) {
   try {
     const connection = await mongoClient.connect(URL);
     const db = connection.db(DB);
@@ -37,7 +39,7 @@ app.get("/users", async function (req, res) {
   }
 });
 
-app.get("/user/:id", async function (req, res) {
+app.get("/treadmillimgs/:id", async function (req, res) {
   try {
     const connection = await mongoClient.connect(URL);
     const db = connection.db(DB);
@@ -51,6 +53,228 @@ app.get("/user/:id", async function (req, res) {
     res.json("somethong went wrong");
   }
 });
+
+
+// out of stcok imgs in treadmill
+app.get("/treadmillimgsoutofstock", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("TreamillOutofStock").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/treadmillimgsoutofstock/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("TreamillOutofStock")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+// cross trainers
+app.get("/crosstrainersimgs", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("CrossTrainers").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/crosstrainersimgs/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("CrossTrainers")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+// crosstrainersimgsoutofstock
+app.get("/crosstrainersimgsoutofstock", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("CrossTrainersOutofStock").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/crosstrainersimgsoutofstock/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("CrossTrainersOutofStock")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+// massagers
+
+app.get("/massagersimgs", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("Massager").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+
+
+app.get("/massagersimgs/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("Massager")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+
+// massager out of stock
+app.get("/massagersimgsoutofstock", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("MassagerOutofStock").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/massagersimgsoutofstock/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("MassagerOutofStock")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+// exercise bikes
+
+app.get("/exercisebikesimgs", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("Exercise Bikes").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/exercisebikesimgs/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("Exercise Bikes")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+// exercise bikes outof stock
+app.get("/exercisebikesimgsoutofstock", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db.collection("ExcerciseBikeOutofStock").find().toArray();
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+app.get("/exercisebikesimgsoutofstock/:id", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    const resUser = await db
+      .collection("ExcerciseBikeOutofStock")
+      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
+    connection.close();
+    res.json(resUser);
+  } catch (error) {
+    console.log(error);
+    res.json("somethong went wrong");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.put("/user/:id", async function (req, res) {
   try {
